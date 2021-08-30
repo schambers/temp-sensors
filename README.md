267032913fd928c2e1d79ef642cbd0c20112b474
schambers <schambers80@gmail.com>
2021-08-29 21:30:53 -0500

    add config, influxdb client and write_points

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # pizero-temp
-PiZeroW Temperature/Humidity sensor script
+Pi Zero W Temperature/Humidity sensor script
 
 Sample script from playing with a Pi Zero W and a DHT22 temperature sensor
 * [Pi Zero W](https://www.raspberrypi.org/products/raspberry-pi-zero-w/)
 * [DHT22 Temperature Sensor](https://www.adafruit.com/product/385)
+
+Python requirements include:
+* `pip install Adafruit_DHR`
+* `pip install influxdb`
+* `pip install --no-cache-dir --no-binary pyyaml pyyaml`
+
+Create a config.yaml in the root of your project that contains the following settings:
+```
+influxdb:
+    host: influxdb_host_or_ip
+    port: influxdb_port
+    database: influxdb_database
+```
+
+I'm sure there's an easier way to document python package requirements but the requirements are minimal here
+I assumed it'd be easier to just list the two required packages.
+
+Sends metrics to InfluxDB, once every 60 seconds in the form of the following document:
+```
+[{
+    "measurement": "temp_event",
+    "tags": {
+        "host": "pizero-garage"
+    },
+    "time": "2021-08-30T01:00:00Z",
+    "fields": {
+        "temperature": 75.4,
+        "humidity": 41
+    }
+}]
+```
--- temp.py
+++ temp.py
@@ -1,14 +1,45 @@
 import Adafruit_DHT
 import time
+import datetime
+import yaml
+from influxdb import InfluxDBClient
 
+# This is the pin and library for a DHT22 temperature sensor
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
+
+# Read yaml config
+file = open('config.yml', 'r')
+cfg = yaml.load(file, Loader=yaml.FullLoader)
+
+# Create the InfluxDB client loading config from secrethub
+client = InfluxDBClient(host=cfg['influxdb']['host'], port=cfg['influxdb']['port'], database=cfg['influxdb']['database'])
 
 while True:
     humidity, temp = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temp is not None:
         tempf = (temp * 9/5) + 32
         print("Temp={0:0.1f}F Temp={1:0.1f}C Humidity={2:0.1f}%".format(tempf, temp, humidity))
+
+        # Create measurement json data
+        json_body = [
+            {
+                "measurement": "temp_event",
+                "tags": {
+                    "host": "pizero-garage"
+                },
+                "time": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                "fields": {
+                    "temperature": tempf,
+                    "humidity": humidity
+                }
+            }
+        ]
+
+        # Send measurement to InfluxDB
+        client.write_points(json_body)
     else:
         print("Sensor failure. Check wiring.");
-    time.sleep(5);
+
+    # Sleep for 60 seconds before sending the next measurement
+    time.sleep(60);
